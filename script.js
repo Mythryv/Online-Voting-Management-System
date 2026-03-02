d4ae74ba8cd58bce687e8e9c7e5efccb5133d17b
Mythryv <119165516+Mythryv@users.noreply.github.com>
2026-03-02 21:13:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,42 @@ let votes = {
     C: 0
 };
 
-let hasVoted = false;
+let loggedInUser = "";
+let votedUsers = [];
+
+function login() {
+    let username = document.getElementById("username").value;
+
+    if (username === "") {
+        alert("Please enter username");
+        return;
+    }
+
+    loggedInUser = username;
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("votingSection").style.display = "block";
+}
 
 function vote(candidate) {
-    if (hasVoted) {
+
+    if (votedUsers.includes(loggedInUser)) {
         alert("You have already voted!");
         return;
     }
+
     votes[candidate]++;
     document.getElementById(candidate).innerText = votes[candidate];
-  
-    hasVoted = true;
+
+    votedUsers.push(loggedInUser);
+    alert("Vote submitted successfully!");
+}
+
+function showResult() {
+    let winner = "A";
+    
+    if (votes.B > votes[winner]) winner = "B";
+    if (votes.C > votes[winner]) winner = "C";
+
+    document.getElementById("resultBox").innerHTML =
+        "Winner is Candidate " + winner + " with " + votes[winner] + " votes!";
 }
